f4f385579281f3540d6364e55709d1abec18c288
lajoc_devs <fisayobolaji7@gmail.com>
2024-03-23 20:55:38 +0100

    api testing

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
-const bibleVerseEl = document.getElementById("bible-verse");
-// bibleVerseEl.innerHTML = "Hi"
+const catFactsEl = document.getElementById("cat-facts");
+const activityEl = document.getElementById("activity");
+const buttonEl = document.querySelector(".button")
+const activityButtonEl = document.querySelector(".activity-button");
+
+// https://bible-api.com/?random=verse
+// https://apis.scrimba.com/bored/api/activity
+// https://catfact.ninja/fact
+// https://dog.ceo/api/breeds/image/random
+
+addEventListener("dblclick", function () {
+	fetch("https://catfact.ninja/fact")
+		.then((response) => response.json())
+		.then((data) => {
+			// console.log(data)
+
+			catFactsEl.innerHTML = data.fact;
+		});
+});
+
+addEventListener("click", function () {
+	fetch("https://apis.scrimba.com/bored/api/activity")
+		.then((response) => response.json())
+		.then((data) => {
+			// console.log(data)
+
+			activityEl.innerHTML = data.activity;
+		});
+});
